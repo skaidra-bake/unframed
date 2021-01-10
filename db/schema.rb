7eb259f6e25b9aee2b879d51689ada71c4c20e1a
Skaidra Bake <skaidrapat@gmail.com>
2021-01-10 20:34:30 +0000

    Add username to users
    This will allow users to sign up and provide a username which will be later used on profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_191656) do
+ActiveRecord::Schema.define(version: 2021_01_10_193636) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 2021_01_10_191656) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
