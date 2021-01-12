20a5231771879e6f78cf5d091357bc2929b2e5a0
Skaidra Bake <skaidrapat@gmail.com>
2021-01-12 22:25:31 +0000

    Add an ability to organize conversations into sections 2
    In conversations controller, each section has its own action. All of them render conversations/index.html.haml view, but have a different number of total conversations, depending on the section selected

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,8 +3,24 @@ class ConversationsController < ApplicationController
         @conversations = current_user.mailbox.conversations
     end
 
+    def inbox
+        @conversations = current_user.mailbox.inbox
+        render action: :index 
+    end
+
+    def sent
+        @conversations = current_user.mailbox.sentbox
+        render action: :index
+    end
+    
+    def trash
+        @conversations = current_user.mailbox.trash
+        render action: :index
+    end
+
     def show
         @conversation = current_user.mailbox.conversations.find(params[:id])
+        @conversation.mark_as_read(current_user)
     end
 
     def new
@@ -12,7 +28,7 @@ class ConversationsController < ApplicationController
     end
 
     def create
-        recipient = User.find(params[:user_id])
+        recipient = User.find(params[:username])
         receipt = current_user.send_message(recipient, params[:body], params[:subject])
         redirect_to conversation_path(receipt.conversation)
     end
