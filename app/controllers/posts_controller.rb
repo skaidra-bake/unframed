47a9cf15d46d620f0ecfad5edeac4a7d9f35b8ae
Skaidra Bake <skaidrapat@gmail.com>
2021-01-12 01:53:03 +0000

    Add very basic image upload

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:description)
+    params.require(:post).permit(:description, :image)
   end
 end
