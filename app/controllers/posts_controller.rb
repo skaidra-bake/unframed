948603c39f0ed6fa3fcf0f6b3182f96117d8d6c3
Skaidra Bake <skaidrapat@gmail.com>
2021-01-12 00:50:51 +0000

    Add a way to create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,27 @@ class PostsController < ApplicationController
   
   before_action :authenticate_user!
 
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description)
   end
 end
--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -4,15 +4,39 @@ RSpec.describe "Posts", type: :request do
 
   let(:user) { create(:user) }
 
-  describe 'GET /new' do
-    before :each do
-      sign_in user
-    end
+  before :each do
+    sign_in user
+  end
 
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/posts'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /new' do
     it 'renders the new page' do
       get '/posts/new'
       expect(response).to have_http_status(:success)
     end
   end
   
+  describe 'POST /create' do
+    it 'creates new post' do
+      post '/posts', params: { post: {description: 'This is a description'} }
+  
+      expect(response).to redirect_to posts_path
+    end
+
+    it 'fails to creates new post' do
+      allow_any_instance_of(Post).to receive(:save).and_return(false)
+
+      post '/posts', params: { post: {description: 'This is a description'} }
+  
+      expect(response).not_to redirect_to posts_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
