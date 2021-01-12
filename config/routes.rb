549b5455b01db208618472815fc982e42b1fbe29
Skaidra Bake <skaidrapat@gmail.com>
2021-01-12 22:16:36 +0000

    Add an ability to organize conversations using sections
    Sections are: inbox, all, sent, and trash. Convarsations can be counted for each section separately. This was possible by splitting the conversations into separate collections in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
-
   resources :posts
   
   resources :conversations do
     resources :messages
+
+    collection do
+      get :inbox
+      get :all, action: :index
+      get :sent
+      get :trash
+    end
   end
+
+  root 'home#index'
 end
