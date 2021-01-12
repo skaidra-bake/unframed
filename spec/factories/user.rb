ca4e0acf8069624807a931816b85449e61828553
Skaidra Bake <skaidrapat@gmail.com>
2021-01-12 00:22:33 +0000

    Add factory for user

---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :user do
+        email { 'test@example.com' } 
+        username { 'example' }
+        password  { "password" }
+        password_confirmation { 'password' }
+    end
+end
