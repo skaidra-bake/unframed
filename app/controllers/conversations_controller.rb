0cbdb7d52c4f05b20ca49db3ac2554870f7fcb40
Skaidra Bake <skaidrapat@gmail.com>
2021-01-13 19:15:19 +0000

    Add a way for user to be able to have group coversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -28,8 +28,8 @@ class ConversationsController < ApplicationController
     end
 
     def create
-        recipient = User.find(params[:username])
-        receipt = current_user.send_message(recipient, params[:body], params[:subject])
+        recipients = User.where(id: params[:user_ids])
+        receipt = current_user.send_message(recipients, params[:body], params[:subject])
         redirect_to conversation_path(receipt.conversation)
     end
 
