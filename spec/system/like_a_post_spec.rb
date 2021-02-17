32a2bd3f4a76d3f04fc88486cfe959a4119e3626
Skaidra Bake <skaidrapat@gmail.com>
2021-02-17 19:28:33 +0000

    Add test for like feature

---
+++ spec/system/like_a_post_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :system do
+    let(:user){ create(:user) }
+    let(:post){ create(:post, description: FFaker::Lorem.sentence)}
+
+    before :each do
+        post
+        login_as(user)
+    end
+
+    describe 'like button' do
+      it "enables me to like a post" do
+        visit post_path(post)
+        click_on 'Like'
+        # expect(page).to have_content(user.username)
+        expect(page).to have_content('Unlike')
+        expect(post.favoritors.count).to eq(1)
+      end
+    end
+  end
