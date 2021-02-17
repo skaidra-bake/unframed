5b2646642556ef9bdf88cd543a138fbf0a78dc96
Skaidra Bake <skaidrapat@gmail.com>
2021-02-17 19:19:05 +0000

    Setup routes for likes controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts do
     resources :comments, module: :posts
+    resource :like, module: :posts, only: [:update]
   end
   
   resources :conversations do
