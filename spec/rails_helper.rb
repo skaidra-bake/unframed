4e2f97a1e1caa0bdb7296730d2cc7e2d85ee4755
Skaidra Bake <skaidrapat@gmail.com>
2021-02-17 19:53:20 +0000

    Configure rails helper to use selenium chrome headless for system tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,7 +70,9 @@ RSpec.configure do |config|
 
   # selenium chromedriver
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :chrome
+    # whenever you want to see the browser, change it to:
+    # driven_by :selenium, using: :chrome
+    driven_by :selenium_chrome_headless, using: :chrome
   end
   # helper for logging in through devise
   config.include Warden::Test::Helpers
