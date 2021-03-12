4ff1b4583915d5390161f70205d0bc01a28147bf
Skaidra Bake <skaidrapat@gmail.com>
2021-03-12 13:54:32 +0000

    Add profile helpers

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,25 @@
 module ProfilesHelper
+  def current_user_profile?
+    current_user == @user
+  end
+
+  def not_current_user_profile?
+    !current_user_profile?
+  end
+
+  def not_following_user?
+    not_current_user_profile? && !current_user.following?(@user)
+  end
+
+  def following_user?
+    not_current_user_profile? && current_user.following?(@user)
+  end
+
+  def show_link_if(condition, name, options = {}, html_options = {}, &block)
+    if condition
+      link_to(name, options, html_options)
+    elsif block_given?
+      block.arity <= 1 ? capture(name, &block) : capture(name, options, html_options, &block)     
+    end
+  end
 end
