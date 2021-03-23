9c34bd92ed851b4480f6a88ed30febb43e5ac41d
Skaidra Bake <skaidrapat@gmail.com>
2021-03-23 00:23:57 +0000

    Fix comment spec

--- spec/system/comment_under_post_spec.rb
+++ spec/system/comment_under_post_spec.rb
@@ -5,6 +5,8 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :system do
   let(:user) { create(:user, password: 'password') }
   let(:post) { create(:post, description: FFaker::Lorem.sentence) }
+  let(:comment) { FFaker::Lorem.sentence }
+
 
   before do
     post
@@ -13,13 +15,9 @@ RSpec.describe 'Users', type: :system do
 
   describe 'comment' do
     it 'enables me to make a comment' do
-      visit posts_path
-      click_on 'Show'
-      find_field(comment[body])
-      comment = FFaker::Lorem.sentence
-      fill_in comment
-      click_on 'Create Comment'
-      expect(page).to have_content(user.username)
+      visit post_path(post)
+      fill_in 'comment_body', with: comment
+      click_on 'Post Comment'
       expect(page).to have_content(comment)
     end
   end
