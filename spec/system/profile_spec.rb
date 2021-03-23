5f0382b6ac55139e3ba226d90098f1dfb1d7a34a
Skaidra Bake <skaidrapat@gmail.com>
2021-03-23 00:41:45 +0000

    Add WIP on profile

--- spec/system/profile_spec.rb
+++ spec/system/profile_spec.rb
@@ -1,24 +1,55 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Profile', type: :system do
-    let(:user){ create(:user) }
-    let(:user2){ create(:user) }
+  let(:user) { create(:user) }
+  let(:user2) { create(:user) }
 
-    context 'with current logged in user' do
-        before :each do
-            login_as(user)
-        end
-
-        it 'has a button "edit profile"' do
-            visit profile_path(user)
-            expect(page).to have_content('Edit profile')
-            expect(page).not_to have_content('.follow')
-        end
-
-        it 'has a button "follow"' do
-            visit profile_path(user2)
-            expect(page).to have_content('Follow')
-            expect(page).not_to have_content('Edit profile')
-        end
+  context 'with current logged in user' do
+    before do
+      login_as(user)
     end
+
+    it 'has a button "edit profile"' do
+      visit profile_path(user)
+      expect(page).to have_content('Edit profile')
+      expect(page).not_to have_content('.follow')
+    end
+
+    context 'with users who do no follow the other user' do
+      before do
+        visit profile_path(user2)
+      end
+
+      it 'has a button "follow"' do
+        expect(page).to have_content('Follow')
+        expect(page).not_to have_content('Edit profile')
+        expect(page).not_to have_content('.unfollow')
+      end
+    end
+
+    context 'with users who follow other user' do
+      before do
+        visit profile_path(user2)
+        click_on 'Follow'
+      end
+
+      it 'has a button "unfollow"' do
+        expect(page).to have_content('Unfollow')
+        expect(page).not_to have_content('Edit profile')
+        expect(page).not_to have_content('.follow')
+      end
+
+      it 'can unfollow that user' do
+        click_on 'Unfollow'
+        sleep 2
+        expect(page).to have_selector('.follow')
+        expect(page).not_to have_content('Edit profile')
+        expect(page).not_to have_content('.unfollow')
+      end
+    end
+
+    pending 'has a friendly user id in the url'
+  end
 end
