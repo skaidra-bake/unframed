83c829e4ca7049a114c9eaa5c66b5dc6343b3b55
Skaidra Bake <skaidrapat@gmail.com>
2021-04-02 16:24:39 +0100

    Add basic validation test for post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,6 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  let(:post) { build(:post) }
+
+  it 'valid' do
+    expect(post).to be_valid
+  end
+
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
   end
